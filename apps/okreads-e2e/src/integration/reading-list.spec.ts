66b992820de7b413a649105cbcb32e9817a761f2
Samir Abdul <85797009+Samir1911@users.noreply.github.com>
2024-08-12 16:08:25 +0530

    E2E testing Done.

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -12,3 +12,48 @@ describe('When: I use the reading list feature', () => {
     );
   });
 });
+
+
+describe('When: I mark the book as Finished.', ()=>{
+  beforeEach(()=>{
+    cy.startAt('/');
+    cy.get('input[type="search"]').type('ruby');
+    cy.get('form').submit();
+    
+    cy.wait(2000).get('[data-testing="add-to-read-button"]').first().click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('.fa-regular')
+      .last()
+      .parents('button')
+      .click();
+  });
+
+  it('Then: Display the finishedDate', ()=>{
+
+    cy.get('.fa-solid')
+      .last()
+      .parents('.reading-list-item')
+      .find('.reading-list-item--details--finished')
+      .should('be.visible')
+      .and('not.be.empty');
+
+    cy.get('.fa-circle-minus')
+      .last()
+      .parents('button')
+      .click();
+
+  });
+
+  it('Then: The Button in Books catalogue should be updated as Finished.', ()=>{
+    cy.wait(2000)
+      .get('[data-testing="add-to-read-button"]')
+      .first()
+      .should('contain', 'Finished');
+    
+    cy.get('.fa-circle-minus')
+      .last()
+      .parents('button')
+      .click();
+    
+  });
+});
